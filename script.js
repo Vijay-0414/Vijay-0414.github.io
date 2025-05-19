0f9077eff948fa8868295abfa53f29b6bdb0e18c
Vijayaperumal L <160581852+Vijay-0414@users.noreply.github.com>
2025-05-19 11:06:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const words = ["Python Developer", "Cloud Engineer", "Devops Engineer"];
+  let wordIndex = 0;
+  let charIndex = 0;
+  let currentWord = '';
+  const typingSpeed = 100;
+  const erasingSpeed = 50;
+  const newWordDelay = 2000;
+
+  function type() {
+      if (charIndex < words[wordIndex].length) {
+          currentWord += words[wordIndex].charAt(charIndex);
+          document.querySelector('.typing-animation').textContent = currentWord;
+          charIndex++;
+          setTimeout(type, typingSpeed);
+      } else {
+          setTimeout(erase, newWordDelay);
+      }
+  }
+
+  function erase() {
+      if (charIndex > 0) {
+          currentWord = currentWord.slice(0, -1);
+          document.querySelector('.typing-animation').textContent = currentWord;
+          charIndex--;
+          setTimeout(erase, erasingSpeed);
+      } else {
+          wordIndex = (wordIndex + 1) % words.length;
+          setTimeout(type, typingSpeed + 1100);
+      }
+  }
+
+  type();
+});
+
+
+
+// Animate progress bars
+const progressBars = document.querySelectorAll('.progress-done');
+
+progressBars.forEach(bar => {
+    setTimeout(() => {
+        bar.style.width = bar.getAttribute('data-done') + '%';
+        bar.style.opacity = 1;
+    }, 500);
+});
+
+// Animate circular skills
+const circles = document.querySelectorAll('.circle');
+
+circles.forEach(circle => {
+    let percent = circle.getAttribute('data-percent');
+    circle.style.setProperty('--percent', percent);
+});
+
+
+// Back to Top button functionality
+const backToTopButton = document.getElementById("backToTop");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+};
+
+backToTopButton.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
